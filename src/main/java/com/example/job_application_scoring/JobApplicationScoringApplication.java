cc7d6d19ce1f02d98db713fa6208dab7e7ab4398
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-03 17:00:22 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/example/job_application_scoring/JobApplicationScoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_application_scoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationScoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job_application_scoring/JobApplicationScoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_application_scoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
