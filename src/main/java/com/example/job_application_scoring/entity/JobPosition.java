d882f9a2d410cebba819f5c87f19da993c2d28f6
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-03 19:31:30 +0300

    JobPosition entity added

---
+++ src/main/java/com/example/job_application_scoring/entity/JobPosition.java
@@ -0,0 +1,28 @@
+package com.example.job_application_scoring.entity;
+
+import com.example.job_application_scoring.enums.SkillName;
+import com.example.job_application_scoring.valueobject.WeightConfig;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class JobPosition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<SkillName> requiredSkills; //enum mu yapmalıyım ayrı bir class mı?
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<SkillName> niceToHaveSkills;
+    @Column(name = "min_experience_year")
+    private Integer minExperienceYear;
+    @Embedded
+    private WeightConfig weights;
+}
