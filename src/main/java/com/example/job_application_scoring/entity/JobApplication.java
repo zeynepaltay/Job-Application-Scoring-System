bf87e14f068f5062437595aa38dbcc94d7633381
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-05 21:57:48 +0300

    many-to-one relationship added

--- src/main/java/com/example/job_application_scoring/entity/JobApplication.java
+++ src/main/java/com/example/job_application_scoring/entity/JobApplication.java
@@ -2,16 +2,23 @@ package com.example.job_application_scoring.entity;
 
 import com.example.job_application_scoring.enums.JobApplicationStatus;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class JobApplication {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long candidateId;
-    private Long jobApplicationId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "candidate_id")
+    private CandidateProfile candidate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "job_position_id")
+    private JobPosition position;
     private LocalDateTime appliedAt;
     private Double score;
     @Enumerated(EnumType.STRING)
