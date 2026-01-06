5b8eca7221d495a7a9bd5c865967618dbb5e8638
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-06 18:52:55 +0300

    WeightConfig features changed to more detailed ones

--- src/main/java/com/example/job_application_scoring/valueobject/WeightConfig.java
+++ src/main/java/com/example/job_application_scoring/valueobject/WeightConfig.java
@@ -1,10 +1,15 @@
 package com.example.job_application_scoring.valueobject;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WeightConfig {
-    double skill;
-    double experience;
-    double education;
+    private int requiredSkillScore;      // 40
+    private int niceToHaveSkillScore;     // 10
+    private int minExperienceScore;       // 30
+    private int extraExperiencePerYear;   // 5
+    private int certificationScore;       //10
 }
