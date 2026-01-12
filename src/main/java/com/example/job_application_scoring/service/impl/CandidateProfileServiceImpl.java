98f5b666252d91400c9eaa4dafce6b7a636ef85d
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-12 23:36:53 +0300

    createProfile method added

--- src/main/java/com/example/job_application_scoring/service/impl/CandidateProfileServiceImpl.java
+++ src/main/java/com/example/job_application_scoring/service/impl/CandidateProfileServiceImpl.java
@@ -1,11 +1,25 @@
 package com.example.job_application_scoring.service.impl;
 
+import com.example.job_application_scoring.dto.responses.CandidateProfileResponse;
+import com.example.job_application_scoring.dto.ruquests.CandidateProfileRequest;
+import com.example.job_application_scoring.entity.CandidateProfile;
+import com.example.job_application_scoring.mapper.CandidateProfileMapper;
 import com.example.job_application_scoring.repository.CandidateProfileRepository;
+import com.example.job_application_scoring.service.CandidateProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CandidateProfileServiceImpl {
+public class CandidateProfileServiceImpl implements CandidateProfileService {
     private final CandidateProfileRepository candidateProfileRepository;
+    private final CandidateProfileMapper candidateProfileMapper;
+
+    //@Transactional olmalı mı?
+    @Override
+    public CandidateProfileResponse createProfile(CandidateProfileRequest candidateProfileRequest) {
+        CandidateProfile entity = candidateProfileMapper.toEntity(candidateProfileRequest);
+        CandidateProfile saved = candidateProfileRepository.save(entity);
+        return candidateProfileMapper.toResponse(saved);
+    }
 }
