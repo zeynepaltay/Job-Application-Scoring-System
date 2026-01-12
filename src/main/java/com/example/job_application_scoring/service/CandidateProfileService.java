f768ecf0b637f5241f1a1797610402f367bc5691
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-12 23:37:03 +0300

    createProfile method added

--- src/main/java/com/example/job_application_scoring/service/CandidateProfileService.java
+++ src/main/java/com/example/job_application_scoring/service/CandidateProfileService.java
@@ -1,4 +1,8 @@
 package com.example.job_application_scoring.service;
 
+import com.example.job_application_scoring.dto.responses.CandidateProfileResponse;
+import com.example.job_application_scoring.dto.ruquests.CandidateProfileRequest;
+
 public interface CandidateProfileService {
+    CandidateProfileResponse createProfile(CandidateProfileRequest candidateProfileRequest);
 }
