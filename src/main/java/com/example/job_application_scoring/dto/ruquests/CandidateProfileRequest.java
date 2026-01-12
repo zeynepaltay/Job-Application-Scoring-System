082e628273ca4138c7f40058acdee63e60a7514b
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-12 23:06:12 +0300

    yearsOfExperience's annotation change to NotNull

--- src/main/java/com/example/job_application_scoring/dto/ruquests/CandidateProfileRequest.java
+++ src/main/java/com/example/job_application_scoring/dto/ruquests/CandidateProfileRequest.java
@@ -2,12 +2,15 @@ package com.example.job_application_scoring.dto.ruquests;
 
 import com.example.job_application_scoring.enums.EducationLevel;
 import com.example.job_application_scoring.enums.SkillName;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Set;
 public class CandidateProfileRequest {
-    @NotBlank(message = "Experience year is a required field")
+    @NotNull(message = "Experience year is required")
+    @Min(value = 0, message = "Experience year cannot be negative")
     private Integer yearsOfExperience;
     @NotEmpty(message = "Skills cannot be empty")
     private Set<SkillName> skills;
