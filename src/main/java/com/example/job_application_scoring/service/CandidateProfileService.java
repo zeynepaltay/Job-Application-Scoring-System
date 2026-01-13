ed180e1dd6e67fd5aa450112b4590988fb570428
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-13 22:48:55 +0300

    deleteProfile added

--- src/main/java/com/example/job_application_scoring/service/CandidateProfileService.java
+++ src/main/java/com/example/job_application_scoring/service/CandidateProfileService.java
@@ -6,4 +6,5 @@ import com.example.job_application_scoring.dto.ruquests.CandidateProfileRequest;
 public interface CandidateProfileService {
     CandidateProfileResponse createProfile(CandidateProfileRequest candidateProfileRequest);
     CandidateProfileResponse updateProfile(CandidateProfileRequest request, Long id);
+    void deleteProfile(Long id);
 }
