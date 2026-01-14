22b6423b8fd51659707ef68b7bf64b296f3c792e
zeynepaltay <altay-zeynep@hotmail.com>
2026-01-14 22:26:10 +0300

    delete update create method added added

--- src/main/java/com/example/job_application_scoring/service/JobPositionService.java
+++ src/main/java/com/example/job_application_scoring/service/JobPositionService.java
@@ -1,5 +1,10 @@
 package com.example.job_application_scoring.service;
 
-public interface JobPositionService {
+import com.example.job_application_scoring.dto.responses.JobPositionResponse;
+import com.example.job_application_scoring.dto.ruquests.JobPositionRequest;
 
+public interface JobPositionService {
+    JobPositionResponse createJobPosition(JobPositionRequest request);
+    JobPositionResponse updateJobPosition(JobPositionRequest request, Long id);
+    void deleteJobPosition(Long id);
 }
